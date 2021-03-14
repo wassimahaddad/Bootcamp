2393df9095b6dada1582e8ad3863ec3fe9915111
Wassim Haddad <wass75@gmail.com>
2021-03-14 15:32:50 +0200

    insert commited

--- Bootcamp_excercises/js/34/todoUI-noLocalStorage/index.js
+++ Bootcamp_excercises/js/34/todoUI-noLocalStorage/index.js
@@ -1,12 +1,11 @@
-// let todoList = [];
-localStorage.setItem("todoList", JSON.stringify([]));
+let todoList = [];
+
 // -----------------------------------------
 // create items
 function addItem() {
   let tName = document.querySelector("#task-text").value;
-  const myData = JSON.parse(localStorage.getItem("todoList"));
   let todo = {};
-  todo.id = myData.length;
+  todo.id = todoList.length;
   todo.task = tName;
   todo.isCompleted = false;
   const date = new Date();
@@ -15,13 +14,11 @@ function addItem() {
   }/${date.getFullYear()}`;
   todo.created = created;
   todo.isImportant = isImportant;
-  // todoList.push(todo);
-
-  myData.push(todo);
+  todoList.push(todo);
   document.querySelector("#task-text").value = "";
   document.querySelector("#important").checked = false;
   isImportant = false;
-  // list();
+  list();
 }
 
 // delete item
@@ -106,13 +103,95 @@ function sortImportant() {
   todoList = sortTempArr;
   list();
 }
-
-function newList() {
-  JSON.parse(localStorage.getItem("todoList")).map((obj, index) => {
-    console.log(obj, index);
-  });
+//sort by date
+function sortByDate() {
+  todoList.sort((a, b) =>
+    a.created > b.created ? 1 : b.created > a.created ? -1 : 0
+  );
+  list();
 }
-// list items
+
+//create delete button
+function listDelete(el) {
+  const deleteTask = document.createElement("button");
+  deleteTask.textContent = "Delete";
+  deleteTask.classList.add("task-actions");
+  deleteTask.addEventListener("click", (e) => DeleteItem(e.target));
+  el.appendChild(deleteTask);
+}
+
+// create edit button
+
+function listEdit(el) {
+  const editTask = document.createElement("button");
+  editTask.textContent = "Edit";
+  editTask.classList.add("task-actions");
+  editTask.addEventListener("click", (e) => editItems(e.target));
+  el.appendChild(editTask);
+}
+
+// create Id number
+function listNumber(el, ob) {
+  const number = document.createElement("div");
+  number.textContent = ob.id + 1;
+  el.appendChild(number);
+}
+
+// create completed checkbox
+function listCB(el, ob) {
+  const check = document.createElement("input");
+  check.type = "checkbox";
+  check.classList.add("checkbox");
+  if (ob.isCompleted === true) {
+    check.checked = true;
+  } else {
+    check.checked = false;
+  }
+  check.addEventListener("change", (e) => checkUncheck(e.target));
+  el.appendChild(check);
+}
+
+// create date entry
+function listCreated(el, ob) {
+  const created = document.createElement("div");
+  created.textContent = ob.created;
+  el.appendChild(created);
+}
+
+// create task name
+function listItem(el, ob) {
+  const item = document.createElement("li");
+  item.textContent = ob.task;
+  item.classList.add(".task-name");
+  el.appendChild(item);
+}
+
+// create important text
+function listImportant(el) {
+  const important = document.createElement("div");
+  important.classList.add("important");
+  important.textContent = "Important:";
+  el.appendChild(important);
+}
+// create important checkbox
+function listImportantCB(el, ob) {
+  const importantCbox = document.createElement("input");
+  importantCbox.type = "checkbox";
+  if (ob.isImportant === true) {
+    importantCbox.checked = true;
+  } else {
+    importantCbox.checked = false;
+  }
+  importantCbox.addEventListener("change", () => {
+    if (importantCbox.checked === true) {
+      ob.isImportant = true;
+    } else {
+      ob.isImportant = false;
+    }
+  });
+  el.appendChild(importantCbox);
+}
+// list tasks
 function list() {
   document.querySelector(".display").innerHTML = "";
   todoList.map((obj) => {
@@ -120,72 +199,20 @@ function list() {
     const displayList = document.querySelector(".display");
     displayList.appendChild(line1);
     line1.className = "line1";
-
-    // delete
-    const deleteTask = document.createElement("button");
-    deleteTask.textContent = "Delete";
-    deleteTask.classList.add("task-actions");
-    deleteTask.addEventListener("click", (e) => DeleteItem(e.target));
-    // edit
-    const editTask = document.createElement("button");
-    editTask.textContent = "Edit";
-    editTask.classList.add("task-actions");
-    editTask.addEventListener("click", (e) => editItems(e.target));
-
-    // completed checkbox
-    const check = document.createElement("input");
-    check.type = "checkbox";
-    check.classList.add("checkbox");
-    if (obj.isCompleted === true) {
-      check.checked = true;
-    } else {
-      check.checked = false;
-    }
-    check.addEventListener("change", (e) => checkUncheck(e.target));
-
-    //task number
-    const number = document.createElement("div");
-    number.textContent = obj.id + 1;
-
-    // task name
-    const item = document.createElement("li");
-    item.textContent = obj.task;
-    item.classList.add(".task-name");
-    // created
-    const created = document.createElement("div");
-    created.textContent = obj.created;
-    // important
-    const important = document.createElement("div");
-    const importantCbox = document.createElement("input");
-    importantCbox.type = "checkbox";
-    if (obj.isImportant === true) {
-      importantCbox.checked = true;
-    } else {
-      importantCbox.checked = false;
-    }
-    importantCbox.addEventListener("change", () => (obj.isImportant = true));
-    important.classList.add("important");
-    important.textContent = "Important:";
-    // append
-    line1.appendChild(deleteTask);
-    line1.appendChild(editTask);
-    line1.appendChild(number);
-    line1.appendChild(check);
-    line1.appendChild(created);
-    line1.appendChild(important);
-    line1.appendChild(importantCbox);
-    line1.appendChild(item);
+    listDelete(line1);
+    listEdit(line1);
+    listNumber(line1, obj);
+    listCB(line1, obj);
+    listCreated(line1, obj);
+    listImportant(line1);
+    listImportantCB(line1, obj);
+    listItem(line1, obj);
   });
 }
-
+// ! listeners
 const create = document.querySelector(".create");
-// const displayList = document.querySelector(".display");
-
-// ! listener
 create.addEventListener("click", addItem);
-
 const important = document.querySelector("#important");
-
 important.addEventListener("change", setImportant);
 const completed = document.querySelector("#sort-completed");
 completed.addEventListener("click", sortCompleted);
@@ -193,10 +220,3 @@ const byImportant = document.querySelector("#sort-important");
 byImportant.addEventListener("click", sortImportant);
 const byDate = document.querySelector("#sort-date");
 byDate.addEventListener("click", sortByDate);
-
-function sortByDate() {
-  todoList.sort((a, b) =>
-    a.created > b.created ? 1 : b.created > a.created ? -1 : 0
-  );
-  list();
-}
