c60e421973b6891e6f331bdb352b74494a200efd
Wassim Haddad <wass75@gmail.com>
2021-03-16 15:42:54 +0200

    insert commited

---
+++ Bootcamp_excercises/react/9.1/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import MyComponent from "./Components/MyComponent";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <MyComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
