eb7ce8659a0c25c0082327d13b6474d8262cb006
Wassim Haddad <wass75@gmail.com>
2021-03-22 16:35:07 +0200

    insert commited

---
+++ Bootcamp_excercises/react/16.1/src/components/LangingPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className="home"> Welcome to the absolutely empty landing page</div>
+  );
+};
+
+export default Homepage;
