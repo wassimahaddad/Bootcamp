314530ff8713e0efb57c82c0781538879a28c4e5
Wassim Haddad <wass75@gmail.com>
2021-03-23 15:54:02 +0200

    insert commited

--- Bootcamp_excercises/react/00Master/src/components/LangingPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Homepage = () => {
-  return (
-    <div className="home"> Welcome to the absolutely empty landing page</div>
-  );
-};
-
-export default Homepage;
--- Bootcamp_excercises/react/00Master/src/components/PageNotFound.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LandingPage = () => {
-  return <div className="home"> Page not found</div>;
-};
-
-export default LandingPage;
