43f76ab1a67e38e1531f912b553a575e8c01bdef
Wassim Haddad <wass75@gmail.com>
2021-04-12 16:58:24 +0300

    insert commited

---
+++ Bootcamp_excercises/Node.js/Ex07/app.js
@@ -0,0 +1,32 @@
+const url = "https://dog.ceo/api/breeds/image/random";
+const axios = require("axios");
+const request = require("request");
+const fetch = require("node-fetch");
+// ------------- axios -------------------------------
+const getDog = async () => {
+  try {
+    const response = await axios.get(url);
+    console.log("provided by axios:", response.data.message);
+  } catch (e) {
+    console.log("unable to connect");
+  }
+};
+
+getDog();
+// ------------- request -----------------------------
+request({ url: url }, (error, response) => {
+  if (error) {
+    console.log("unable to connect");
+  } else {
+    const data = JSON.parse(response.body);
+    console.log("provided by request:", data.message);
+  }
+});
+// ------------- 3rd party ---------------------------
+
+fetch(url)
+  .then((res) => res.text())
+  .then((body) =>
+    console.log("provided by node-fetch:", JSON.parse(body).message)
+  )
+  .catch((err) => console.log("unable to connect"));
