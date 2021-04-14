6710174a7b8a368e0ddfe02ca572e81da9007c88
Wassim Haddad <wass75@gmail.com>
2021-04-14 22:11:10 +0300

    insert commited

--- Bootcamp_excercises/Node.js/Ex11/Utils/Utils.js
+++ Bootcamp_excercises/Node.js/Ex11/Utils/Utils.js
@@ -5,32 +5,27 @@ const getMovies = () => {
 };
 const getMovie = (id) => {
   const arr = JSON.parse(getMovies().toString());
-  const movie = arr.filter((obj) => obj.id === parseInt(id));
-  return movie;
+  const movie = arr.filter((obj) => obj.id == id);
+  return movie[0];
 };
 
 const addMovie = (movie) => {
   let arr = JSON.parse(getMovies().toString());
   arr.push(movie);
-  console.log(arr);
   fs.writeFileSync("./Database/movies.json", JSON.stringify(arr));
 };
 
 const removeMovie = (id) => {
   // console.log(id);
   const arr = JSON.parse(getMovies().toString());
-  let newArr = arr.filter((obj) => obj.id !== parseInt(id));
+  let newArr = arr.filter((obj) => obj.id != id);
   fs.writeFileSync("./Database/movies.json", JSON.stringify(newArr));
 };
 
 const updateMovie = (id, data) => {
-  const arr = JSON.parse(getMovies().toString());
-  const movie = arr.filter((obj) => obj.id === parseInt(id));
-  let updated = { ...movie[0], ...data };
-  const newArr = JSON.parse(getMovies().toString()).filter(
-    (obj) => obj.id !== parseInt(id)
-  );
-  newArr.push(updated);
-  fs.writeFileSync("./Database/movies.json", JSON.stringify(newArr));
+  const movie = getMovie(id);
+  let updated = { ...movie, ...data };
+  removeMovie(id);
+  addMovie(updated);
 };
 module.exports = { getMovies, getMovie, addMovie, removeMovie, updateMovie };
--- Bootcamp_excercises/Node.js/Ex11/app.js
+++ Bootcamp_excercises/Node.js/Ex11/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const chalk = require("chalk");
 
 const app = express();
 const {
@@ -20,30 +21,42 @@ app.get("/api/movies/:id", (request, response) => {
   const { id } = request.params;
   const movie = getMovie(id);
   response.send(movie);
-  console.log("user requested movie by id");
+  console.log(chalk.green.inverse(`user requested movie with ID ${id}`));
 });
 
 app.post("/api/movies", (request, response) => {
   try {
     const newMovie = addMovie(request.body);
-    response.status(201).send(newMovie);
+    response.status(201).send("New Movie was successfully created");
+    console.log(chalk.green.inverse("user created new movie"));
   } catch (e) {
     response.status(400).send({ error: e.message });
   }
 });
 
 app.put("/api/movies/:id", (request, response) => {
-  const { id } = request.params;
-  const movie = request.body;
-  // console.log(movie);
-  updateMovie(id, movie);
-  response.send(`Movie data was updated`);
+  try {
+    const { id } = request.params;
+    const movie = request.body;
+    updateMovie(id, movie);
+    response.status(200).send(`Movie with ID ${id} was updated`);
+    console.log(chalk.green.inverse(`user updated movie with ID ${id}`));
+  } catch (e) {
+    response.status(400).send({ error: e.message });
+  }
 });
 
 app.delete("/api/movies/:id", (request, response) => {
-  const { id } = request.params;
-  removeMovie(id);
-  response.send(`Movie with ID ${id} was deleted`);
+  try {
+    const { id } = request.params;
+    removeMovie(id);
+    response.status(200).send(`Movie with ID ${id} was deleted`);
+    console.log(
+      chalk.green.inverse(`user deleted movie with ID ${id} was deleted`)
+    );
+  } catch (e) {
+    response.status(400).send({ error: e.message });
+  }
 });
 
 const PORT = 3000;
